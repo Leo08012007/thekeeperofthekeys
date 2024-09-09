47073ce4dfa3dd53a3eeda02211be67aed261ba4
Leo08012007 <mokshithatailuru@gmail.com>
2024-09-10 00:09:37 +0530

    Changed Name to Keeper of the keys

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "📚 Curiosity Chronicles",
+    pageTitle: "🔑 The Keeper Of The Keys",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
